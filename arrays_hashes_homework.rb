2de5b955d17860b186085dc95c19fa9f3a80ff3b
StuartCharters <charters.stuart@gmail.com>
2018-05-23 16:20:25 +0100

    8 (2 results) done

--- arrays_hashes_homework.rb
+++ arrays_hashes_homework.rb
@@ -25,10 +25,15 @@ stops.delete("Livingston")
 stops.delete_at(2)
 
 #7. How many stops there are in the array?
-p stops.length
+length_of_array = stops.length
 
 #8. How many ways can we return `"Falkirk High"` from the array?
+p stops[2]
+p stops[-5]
+p
+
 #9. Reverse the positions of the stops in the array
+
 #10. Print out all the stops using a for loop
 
 # Exercise B
