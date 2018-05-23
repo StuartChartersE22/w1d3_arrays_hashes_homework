a97de907a2fb6ea523c1db6b72089833d2e7a76f
StuartCharters <charters.stuart@gmail.com>
2018-05-23 18:06:45 +0100

    c2 done, numbers easier to read

--- arrays_hashes_homework.rb
+++ arrays_hashes_homework.rb
@@ -167,17 +167,17 @@ users["Stuart"] = {
 united_kingdom = [
   {
     name: "Scotland",
-    population: 5295000,
+    population: 5_295_000,
     capital: "Edinburgh"
   },
   {
     name: "Wales",
-    population: 3063000,
+    population: 3_063_000,
     capital: "Swansea"
   },
   {
     name: "England",
-    population: 53010000,
+    population: 53_010_000,
     capital: "London"
   }
 ]
@@ -191,6 +191,17 @@ for country in united_kingdom
   end
 end
 
+p united_kingdom[1][:capital]
+
+#alt:
+united_kingdom[1][:capital] = "Cardiff"
+
 #2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+united_kingdom << {
+  name: "Northern Ireland",
+  population: 1_811_000,
+  capital: "Belfast"
+}
+
 #3. Use a loop to print the names of all the countries in the UK.
 #4. Use a loop to find the total population of the UK.
